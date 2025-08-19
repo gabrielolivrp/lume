1128b8157081989ed9064d704a0655fa2d32d643
Gabriel Augusto <gabrielolivrp@gmail.com>
2025-08-19 18:31:05 -0300

    cleanup

--- lume-core/test/Main.hs
+++ lume-core/test/Main.hs
@@ -1,4 +1,4 @@
-import Test.Tasty (TestTree, defaultMain, testGroup)
+import Test.Tasty (defaultMain, testGroup)
 
 import Lume.Core.Block.DifficultySpec (difficultySpec)
 import Lume.Core.Block.InternalSpec (blockInternalSpec)
